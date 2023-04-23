636369e344398036c3a65f9ac8d7e561d886efbc
MatheusJunior2334 <124627522+MatheusJunior2334@users.noreply.github.com>
2023-04-23 13:08:44 -0300

    Novas adições na Section1

--- src/Components/Section1.jsx
+++ src/Components/Section1.jsx
@@ -4,7 +4,15 @@ function Section1() {
     return (
         <section id={style.section1}>
             <article>
-                <div id={style.section1_part1}></div>
+                <div id={style.section1_part1}>
+                    <div id={style.main_text}>
+                        <h1>Venha <span className={style.text_color}>facilitar</span> e <span className={style.text_color}>otimizar</span> as suas finanças conosco</h1>
+                        <p>Descubra melhores maneiras de agilizar as suas transações econômicas em todos os sentidos</p>
+                    </div>
+                    <div id={style.main_button}>
+                        <button>Abra sua Conta</button>
+                    </div>
+                </div>
                 <div id={style.section1_part2}></div>
             </article>
         </section>
